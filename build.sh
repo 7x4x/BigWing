f4bc8687b757473eb9b160ee3175e7fe27c8d94f
7x4x <donot0waste0your0time@gmail.com>
2022-06-23 19:07:21 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-mkdir dist dist/assets dist/assets/css dist/assets/favicon dist/assets/js dist/assets/images dist/assets/languages dist/views dist/views/includes dist/views/layout
+#!/bin/bash
+mkdir dist dist/{assets,views} dist/assets/{css,favicon,js,images,languages} dist/views/{includes,layout}
 touch dist/views/index.pug
-mkdir src src/js src/languages src/sass src/sass/abstracts src/sass/base src/sass/components src/sass/layout src/sass/pages src/sass/themes src/sass/vendors
-touch src/sass/main.sass src/sass/base/_normalize.sass src/sass/base/_global.sass src/sass/abstracts/_variables.sass src/sass/abstracts/_helpers.sass src/sass/abstracts/_colors.sass src/sass/abstracts/_mixins.sass src/sass/layout/_header.sass src/sass/layout/_grid.sass src/sass/layout/_footer.sass
+mkdir src src/{js,languages,sass} src/sass/{abstracts,base,components,layout,pages,themes,vendors}
+touch src/sass/main.sass src/sass/base/{_normalize.sass,_global.sass} src/sass/abstracts/{_variables.sass,_helpers.sass,_colors.sass,_mixins.sass} src/sass/layout/{_header.sass,_grid.sass,_footer.sass}
 echo node_modules >> .gitignore
